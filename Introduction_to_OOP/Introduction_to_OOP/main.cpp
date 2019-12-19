2d9d23ca5fe12cdf971f3388676d4f9a13197b5f
Student <Student@304-1>
2019-12-19 20:54:50 +0200

    копирование, присваевание

--- Introduction_to_OOP/Introduction_to_OOP/main.cpp
+++ Introduction_to_OOP/Introduction_to_OOP/main.cpp
@@ -13,23 +13,67 @@ public:
 	{
 		return y;
 	}
-	void set_x(double x)
+	void set_x(double x) //const
 	{
-		this->x = x;
+		if (/*x >= 100 && x <= 100*/ abs(x) <= 100)
+		{
+			this->x = x;
+		}
+		else
+		{
+			this->x = 0;
+		}
+		//if фильтрует некоректные згачения
 	}
 	void set_y(double y)
 	{
 		this->y = y;
 	}
+	//		Constructor's:
+	/*Point()
+	{
+		x = y = 0;
+		std::cout << "DefaultConstructor:\t" << this << std::endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		std::cout << "DefaultConstructor:\t" << this << std::endl;;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		std::cout << "CopyConstructor: " << this << std::endl;
+	}
+	~Point()
+	{
+		std::cout << "Destructor:\t" << this << std::endl;
+	}
+	//Operators
+	void operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		std::cout << "CoppyAssignment:\t" << this << std::endl;
+	}
+
+	//		Methods
+	void print() const
+	{
+		std::cout << "x = " << x << "\t" << "y = " << y << std::endl;
+	}
 };
 
-//#define basics
-#define enncapsulation
+//#define BASICS
+//#define ENCAPSULATION
+//#define CONSTRUCTORS
 
 void main()
 {
 	setlocale(LC_ALL, "");
-#ifdef basics
+#ifdef BASICS
 	int a;		//обявление переменной 'a', типа 'int'.
 	Point A;	//обявление переменной 'A', типа 'Point'
 				//Создание(обявление) обекта 'A' типа 'Point'.
@@ -40,7 +84,7 @@ void main()
 	Point* pA = &A; //Обявление указателя на 'Point' 'pA' на адрес 'A'
 	std::cout << pA->x << "\t" << pA->y << std::endl;
 	std::cout << (*pA).x << "\t" << (*pA).y << std::endl;
-#endif // basics
+#endif // BASICS
 	/*
 		'.'	 -	Оператор прямого доступа (Point operator),
 				используется для обращения к полям (членам) обекта по имени обекта
@@ -50,11 +94,41 @@ void main()
 
 	*/
 
-#ifdef enncapsulation
+#ifdef ENCAPSULATION
 	Point A;
 	A.set_x(3);
 	A.set_y(5);
 	std::cout << A.get_x() << "\t" << A.get_y() << std::endl;
 #endif // enncapsulation
+#ifdef CONSTRUCTORS
+	const Point B; //Обявление 
+	//std::cout << B.get_x() << std::endl;
+	B.print();
+	
+	Point C = 5;
+	C.print();
+
+	Point D(6);
+	D.print();
+
+	Point E(5, 2);
+	E.print();
+
+	//Copy construct
+	Point F = E;
+	F.print();
+	Point G;
+	G = E;
+	G.print();
+
+
+#endif // CONSTRUCTORS
+
+	Point A(5, 2);
+	A.print();
+	Point B;	//Default constructor
+	B = A;		//Copy Assignment
+	B.print();
+	//Copy assignment
 
 }
